51ccc785482863cf2ee91446cd1fe72ebae760c7
Rron Shabani <rron.shabani@gmail.com>
2023-04-22 19:24:11 +0200

    first production draft

--- client/src/Components/common/Header.tsx
+++ client/src/Components/common/Header.tsx
@@ -5,22 +5,25 @@ import SocialMediaButtons from "./SocialMediaButtons"
 const Header = () => {
   return (
     <div className="top-bar">
-      <div className="left-section">
-        <div className="top-menu-page">
-          <a href="/">
-          <h2> Home </h2>
-          </a>
+      <div className="header-content">
+        <div className="left-section">
+          <div className="top-menu-page">
+            <a href="/">
+              <h2> Home </h2>
+            </a>
+          </div>
+          <div className="top-menu-page">
+            <a href="/events">
+              <h2> Events </h2>
+            </a>
+          </div>
         </div>
-        <div className="top-menu-page">
-          <a href="/events">
-          <h2> Events </h2>
-          </a>
+        <div className="center-section">
+          Kleine Logo
         </div>
-      </div>
-      <div className="center-section">
-      </div>
-      <div className="right-section">
-        <SocialMediaButtons />
+        <div className="right-section">
+          <SocialMediaButtons />
+        </div>
       </div>
     </div>
   )
